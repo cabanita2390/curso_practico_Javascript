49617d95e7390f175e4960a940f9363c516a380c
Felipe Cabana <cabanita2390@hotmail.com>
2023-05-25 16:25:54 -0500

    Commit 3. Vamos en add event listener

--- script.js
+++ script.js
@@ -1,37 +1,19 @@
-const h1 = document.querySelector('h1');
-const p = document.querySelector('p');
-const parrafito = document.getElementsByClassName('.parrafito');
-const pid = document.getElementById('pid');
-const input = document.querySelector('input');
+const h1 = document.getElementById('h1_verde');
+// console.log(h1);
 
-console.log({
+const input_calculo1 = document.getElementById('input_calculo1');
+const input_calculo2 = document.querySelector('.input_calculo2');
+
+const btnCalcular = document.getElementById('btnCalcular');
+
+console.log(
     h1,
-    p,
-    parrafito,
-    pid,
-    input
-});
+    input_calculo1,
+    input_calculo2,
+    btnCalcular
+);
 
+// const img = document.createElement('img');
+// console.log(img);
 
-h1.innerHTML='Patito <br>Feo';
-
-// console.log(h1.getAttribute('class'));
-// h1.setAttribute('class', 'rojo');
-
-h1.classList.add('rojo');
-
-h1.classList.remove('verde');
-
-input.value='456';
-
-
-const img = document.createElement('img');
-
-img.setAttribute('src', 'https://cdn.pixabay.com/photo/2016/11/19/14/00/code-1839406_1280.jpg');
-
-console.log(img);
-
-pid.innerHTML = '';
-pid.append(img);
-
-console.log(pid);
+// input_calculo1.append(img);
