f2b2860d75c55273e2fb6f1b2656f97d1bf3415a
Felipe Cabana <cabanita2390@hotmail.com>
2023-05-26 16:35:17 -0500

    terminamos addeventlistener

--- script.js
+++ script.js
@@ -1,17 +1,12 @@
+const h1 = document.getElementById("h1_verde");
+const input_calculo1 = document.querySelector("#input_calculo1");
+const input_calculo2 = document.querySelector("#input_calculo2");
+const btnCalcular = document.querySelector("#btnCalcular");
+const result = document.querySelector("#result");
 
-const h1 = document.getElementById('h1_verde');
-const input_calculo1 = document.querySelector('#input_calculo1');
-const input_calculo2 = document.querySelector('#input_calculo2');
-const btnCalcular = document.querySelector('#btnCalcular');
-const result = document.querySelector('#result');
+btnCalcular.addEventListener("click", botonClic);
 
-function botonClic(){
-    const sumaInputs = input_calculo1.value + input_calculo2.value;
-    result.innerHTML = sumaInputs;
+function botonClic() {
+  const sumaInputs = input_calculo1.value + input_calculo2.value;
+  result.innerHTML = sumaInputs;
 }
-
-
-input_calculo2.addEventListener('change', cambiarInput2)
-btnCalcular.addEventListener('click', botonClic);
-
-console.log('prueba');
