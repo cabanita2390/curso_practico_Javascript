7ef23f7b372a50d5267a98698ad01e1bc50e5943
Felipe Cabana <cabanita2390@hotmail.com>
2023-05-27 21:44:37 -0500

    Commit con btncalcular2

--- script.js
+++ script.js
@@ -4,9 +4,30 @@ const input_calculo2 = document.querySelector("#input_calculo2");
 const btnCalcular = document.querySelector("#btnCalcular");
 const result = document.querySelector("#result");
 
+///////////////////
+
 btnCalcular.addEventListener("click", botonClic);
 
 function botonClic() {
-  const sumaInputs = input_calculo1.value + input_calculo2.value;
-  result.innerHTML = sumaInputs;
+  console.log('Clic en el botón de calcular');
+}
+
+//////////////////
+
+const btnCalcular1 = document.querySelector("#btnCalcular1");
+
+btnCalcular1.addEventListener("dblclick", botonClic1);
+
+function botonClic1() {
+  console.log('Doble clic en el botón de calcular');
+}
+
+//////////////////
+
+const btnCalcular2 = document.querySelector("#btnCalcular1");
+
+btnCalcular1.addEventListener("dblclick", botonClic1);
+
+function botonClic1() {
+  console.log('Doble clic en el botón de calcular');
 }
